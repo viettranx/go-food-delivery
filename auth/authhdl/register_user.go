006ac33ef471cef7d5b15f76fad5df188ecab0c4
Cesc Nguyen <thuocnv1802@gmail.com>
2020-11-23 15:18:08 +0700

    auth module

---
+++ auth/authhdl/register_user.go
@@ -0,0 +1,34 @@
+package authhdl
+
+import (
+	"fooddlv/auth/authmodel"
+	"fooddlv/auth/authrepo"
+	"fooddlv/common"
+	"fooddlv/user/userstorage"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Register(appCtx common.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		db := appCtx.GetDBConnection()
+		var user authmodel.CreateUser
+
+		if err := c.ShouldBind(&user); err != nil {
+			c.JSON(http.StatusBadRequest, common.ErrInvalidRequest(err))
+			return
+		}
+
+		store := userstorage.NewUserMysql(db)
+		repo := authrepo.NewAuthRepo(store)
+
+		userId, err := repo.Register(c.Request.Context(), &user)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, err)
+			return
+		}
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(userId > 0))
+	}
+}
---
+++ auth/authrepo/register_user.go
@@ -0,0 +1,42 @@
+package authrepo
+
+import (
+	"context"
+	"fooddlv/auth/authmodel"
+	"fooddlv/common"
+	"fooddlv/hash"
+	"fooddlv/randx"
+	"fooddlv/user/usermodel"
+)
+
+type RegisterStorage interface {
+	FindUserByCondition(ctx context.Context, conditions map[string]interface{}, relations ...string) (*usermodel.User, error)
+	Create(ctx context.Context, createUserData *usermodel.CreateUser) (int, error)
+}
+
+type registerRepo struct {
+	store RegisterStorage
+}
+
+func NewAuthRepo(store RegisterStorage) *registerRepo {
+	return &registerRepo{store: store}
+}
+
+func (repo *registerRepo) Register(ctx context.Context, createUserData *authmodel.CreateUser) (userId int, err error) {
+	user, err := repo.store.FindUserByCondition(ctx, map[string]interface{}{"email": createUserData.Email})
+
+	if user != nil {
+		return 0, common.ErrUserAlreadyExists(authmodel.EntityName, err)
+	}
+
+	md5Hash := hash.NewMd5Hash(createUserData.Password, randx.GenSalt(50))
+
+	userId, err = repo.store.Create(ctx, createUserData.ToCreateUser(md5Hash))
+
+	if err != nil {
+		return 0, common.ErrCannotCreate(authmodel.EntityName, err)
+	}
+
+	return userId, nil
+
+}
